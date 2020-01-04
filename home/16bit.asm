f5a105628cba2e6ed4edd9284f08e5aa6e989844
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2020-01-04 20:36:28 -0300

    Add generic 16-bit function macros for home bank calls

---
+++ home/16bit.asm
@@ -0,0 +1,52 @@
+___conversion_table_homecall: MACRO
+	; macro arguments: homecall type, function label
+	; all functions clobber af and hl (except for outputs) and preserve bc and de
+	; homecall types:
+	; - read: index to ID conversion (in: a = 8-bit ID; out: hl = 16-bit index)
+	; - write: ID to index conversion (in: hl = 16-bit index; out: a = 8-bit ID)
+	; - lock: ID locking (in: a = ID (or zero to unlock), l = position; out: a = preserved)
+	if strcmp("\1", "read") && strcmp("\1", "write") && strcmp("\1", "lock")
+		fail "16-bit homecall: invalid call type"
+	endc
+
+	if "\1" != "write"
+		ld h, a
+	endc
+	ldh a, [hROMBank]
+	push af
+	ld a, BANK(\2)
+	rst Bankswitch
+	if "\1" == "read"
+		ld a, h
+	endc
+	call \2
+	if "\1" != "read"
+		ld l, a
+	endc
+	pop af
+	rst Bankswitch
+	if "\1" != "read"
+		ld a, l
+	endc
+	ret
+ENDM
+
+___conversion_table_homecall_readlocked: MACRO
+	; macro argument: table name
+	; in: a = position
+	; out: a = 8-bit index; everything else preserved
+	push hl
+	add a, LOW(\1LockedEntries)
+	ld l, a
+	ldh a, [rSVBK]
+	ld h, a
+	ld a, BANK(\1LockedEntries)
+	ldh [rSVBK], a
+	ld a, h
+	ld h, HIGH(\1LockedEntries)
+	ld l, [hl]
+	ldh [rSVBK], a
+	ld a, l
+	pop hl
+	ret
+ENDM
